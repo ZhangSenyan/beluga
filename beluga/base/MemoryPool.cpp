a5885797e6396fc2331f7fb9e6b1e056664f28a8
SenyanZhang <senyanzhang.sdcn@gmail.com>
2019-07-23 17:04:59 +0800

    update logging

--- beluga/base/MemoryPool.cpp
+++ beluga/base/MemoryPool.cpp
@@ -1,41 +1,79 @@
-//
-// Created by zhsy on 19-6-8.
-//
+/**
+ * @author Zhang Senyan
+ * Date: 2019-06-07
+ *
+ */
+
 #include <iostream>
 #include "MemoryPool.h"
+
+//构造函数
 MemoryPool::MemoryPool(){
-    std::cout<<"MemoryPool::MemoryPool()"<<std::endl;
+
+    // 申请16个内存块，放入池中
     for(int i=0;i<16;i++){
         _blockArray[i]=new MemoryBlock((i+1)*8);
     }
+
 }
+
+//析构函数
 MemoryPool::~MemoryPool(){
 
 }
 
-
+//单例-饿汉
 MemoryPool& MemoryPool::instance(){
     static MemoryPool memoryPool;
     return memoryPool;
 }
 
+/**
+ * 申请指定大小内存
+ * 不需要加锁，锁在单个内存块中，一定程度能够缓解锁的争用
+ *
+ *  @param : size 申请内存的大小
+ *
+ *  @return ： 申请内存首地址
+ *
+ */
 void* MemoryPool::alloc(size_t size){
 
     if(size>=128){
+        //向系统申请内存
         MemoryUnit* p= (MemoryUnit*)::malloc(size+sizeof(MemoryUnit));
+
+        //所属的内存块为空
         p->holder= nullptr;
         p->next= nullptr;
+
+        //后移8个字节，返回指针
         return ((char*)p+sizeof(MemoryUnit));
     }
 
+    /**
+     * 向内存池申请内存
+     * size/8 -> 确定目标内存块
+     * 向目标内存块申请内存
+     */
     return _blockArray[size/8]->alloc();
 }
 
+/**
+ * 释放内存块
+ *
+ *  @param : void* 要释放的内存首地址
+ *
+ */
 void MemoryPool::free(void *p){
+    //指针前移，获取头信息
     MemoryUnit* memoryUnit=(MemoryUnit*)((char*)p-sizeof(MemoryUnit));
+
     if(memoryUnit->holder)
+        //放到内存块中
         memoryUnit->holder->dealloc(p);
     else
+        //释放内存
         ::free(memoryUnit);
 }
 
--- beluga/base/MemoryPool.h
+++ beluga/base/MemoryPool.h
@@ -1,27 +1,56 @@
-//
-// Created by zhsy on 19-6-8.
-//
+/**
+ * @author Zhang Senyan
+ * Date: 2019-06-07
+ *
+ * 内存池：
+ *    包含16种规格 (1-16)×8 的内存块
+ *    size：申请内存的大小
+ *    if size>128:
+ *      调用系统函数malloc
+ *    else:
+ *      挑选内存块
+ *      申请一个内存单元
+ * 单例-饿汉
+ */
 
-#ifndef HCCSERVER_MEMORYPOOL_H
-#define HCCSERVER_MEMORYPOOL_H
+
+#ifndef BELUGA_MEMORYPOOL_H
+#define BELUGA_MEMORYPOOL_H
 
 #include "MemoryBlock.h"
 
 class MemoryPool {
 private:
+    //构造函数 - 单例
     MemoryPool();
-    ~MemoryPool();
-public:
-    static MemoryPool& instance();
-    void* alloc(size_t size);
-    void free(void *p);
-    void* operator new(size_t size);
-    void operator delete(void* p) noexcept ;
-private:
-    MemoryBlock* _blockArray[16];
 
+    //析构函数
+    ~MemoryPool();
+
+public:
+    // get 内存池
+    static MemoryPool& instance();
+
+    // 申请大小为 size 的内存
+    void* alloc(size_t size);
+
+    //释放 p 指向的内存
+    void free(void *p);
+
+    //构造MemoryPool时需要调用系统malloc
+    void* operator new(size_t size);
+
+    //析构MemoryPool时需要调用系统free
+    void operator delete(void* p) noexcept ;
+
+private:
+
+    //内存块池
+    MemoryBlock* _blockArray[16];
 
 };
 
 
-#endif //HCCSERVER_MEMORYPOOL_H
+#endif //BELUGA_MEMORYPOOL_H
+
+
